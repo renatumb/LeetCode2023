91a380875b10aa45be9f5967f7013c86095f821c
renatu <renatu_mb@yahoo.com.br>
2023-02-01 12:08:38 -0800

    // https://leetcode.com/problems/isomorphic-strings/

--- src/IsIsomorphic.java
+++ src/IsIsomorphic.java
@@ -16,11 +16,31 @@ public class IsIsomorphic {
                 alreadyMapped.put(tt, ss);
                 continue;
             }
-            if ( (      mapping.containsKey(ss) && (char)       mapping.get(ss) != tt) ||
-                 (alreadyMapped.containsKey(tt) && (char) alreadyMapped.get(tt) != ss) ) {
+            if ((mapping.containsKey(ss) && (char) mapping.get(ss) != tt) ||
+                    (alreadyMapped.containsKey(tt) && (char) alreadyMapped.get(tt) != ss)) {
                 return false;
             }
         }
         return true;
     }
+
+    //
+    public static boolean isIsomorphic2(String s, String t) {
+        //final int ascii = 65;
+        char ss[] = new char[256];
+        char tt[] = new char[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            if (ss[s.charAt(i) ] == '\u0000' && tt[t.charAt(i) ] == '\u0000') {
+                ss[s.charAt(i) ] = t.charAt(i);
+                tt[t.charAt(i) ] = s.charAt(i);
+                continue;
+            }
+            if( ss[s.charAt(i) ] != t.charAt(i) || tt[t.charAt(i) ] != s.charAt(i) ){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
