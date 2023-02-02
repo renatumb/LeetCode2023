8af682e6c4585c65f4bd37a743306b8fb40bfe06
renatu <renatu_mb@yahoo.com.br>
2023-02-02 12:37:26 -0800

    // https://leetcode.com/problems/merge-two-sorted-lists

---
+++ src/MergeTwoSortedLists.java
@@ -0,0 +1,60 @@
+// https://leetcode.com/problems/merge-two-sorted-lists
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        if (list1 == null && list2 == null) return null;
+
+        ListNode head;
+        if (list1.val < list2.val) {
+            head = list1;
+            list1 = list1.next;
+        } else {
+            head = list2;
+            list2 = list2.next;
+        }
+        ListNode current = head;
+
+        while (list1 != null | list2 != null) {
+
+            if (list1 == null) {
+                current.next = list2;
+                break;
+            }
+            if (list2 == null) {
+                current.next = list1;
+                break;
+            }
+
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        return head;
+    }
+}
+
