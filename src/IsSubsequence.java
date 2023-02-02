9c9b12014e08f2e1ba2ae8b4c1f0385d138660ec
renatu <renatu_mb@yahoo.com.br>
2023-02-02 00:17:08 -0800

    // https://leetcode.com/problems/is-subsequence/

---
+++ src/IsSubsequence.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/is-subsequence/
+public class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int indexS = 0;
+        String copyS = "";
+        if (s.length() == 0) return true;
+        for (int i = 0; i < t.length(); i++) {
+            if (t.charAt(i) == s.charAt(indexS)) {
+                copyS += t.charAt(i);
+                indexS++;
+                if (copyS.length() == s.length() && copyS.equals(s)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
