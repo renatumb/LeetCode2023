6517b6e650a09a72b24a7a59e2f143dee9b9fa33
renatu <renatu_mb@yahoo.com.br>
2023-03-28 14:13:06 -0700

    // https://leetcode.com/problems/longest-substring-without-repeating-characters

--- src/LongestSubstringWithoutRepeatingCharacters.java
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class LongestSubstringWithoutRepeatingCharacters {
@@ -8,6 +9,10 @@ public class LongestSubstringWithoutRepeatingCharacters {
         System.out.println(lengthOfLongestSubstring2("abcabcbb"));
         System.out.println(lengthOfLongestSubstring2("bbbbb"));
         System.out.println(lengthOfLongestSubstring2("pwwkew"));
+        System.out.println("---");
+        System.out.println(lengthOfLongestSubstring3("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring3("bbbbb"));
+        System.out.println(lengthOfLongestSubstring3("pwwkew"));
     }
 
     public static int lengthOfLongestSubstring1(String s) {
@@ -40,6 +45,22 @@ public class LongestSubstringWithoutRepeatingCharacters {
             }
             set.add(as[end]);
             max = set.size() > max ? set.size() : max;
+        }
+        return max;
+    }
+
+    public static int lengthOfLongestSubstring3(String s) {
+        int max = 0;
+        boolean[] allChars = new boolean[255];
+        char[] as = s.toCharArray();
+        for (int ini = 0, end = 0; end < as.length; end++) {
+            if (allChars[as[end]]) {
+                allChars = new boolean[255];
+                end = ++ini;
+            }
+            allChars[as[ini]] = true;
+            allChars[as[end]] = true;
+            max = (end - ini) + 1 > max ? (end - ini) + 1 : max;
         }
         return max;
     }
